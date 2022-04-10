0393f054b14e262b3404d82df916a88f5a8468ad
Maxim <smn200@mail.ru>
2022-04-10 22:17:12 +1000

    Buy alcohol

--- Operator if/src/Operator/IfElse.java
+++ Operator if/src/Operator/IfElse.java
@@ -1,8 +1,9 @@
 package Operator;
 
+import java.util.Date;
 import java.util.Scanner;
 
-public class Operator {
+public class IfElse {
     public static void main(String[] args){
         System.out.println("Введите ваш возраст");
         int age = new Scanner(System.in).nextInt();
@@ -10,8 +11,10 @@ public class Operator {
         int time = new Scanner(System.in).nextInt();
         if (age < 18){
             System.out.println("Нельзя");
-        } else if(age > 18 && age < 100 && time < 22){
+        } else if(age > 18 && age < 100 && time < 22 ) {
             System.out.println("Можно");
+        } else if(age > 18 && age < 100 && time >= 22) {
+            System.out.println("нельзя");
         } else System.out.println("Ты пьян, иди домой");
     }
 }
