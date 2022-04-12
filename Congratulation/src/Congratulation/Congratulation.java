2bd5faafaf67ee98543fcf4f70d0655589043b6d
Maxim <smn200@mail.ru>
2022-04-12 22:40:22 +1000

    Congratulation

---
+++ Congratulation/src/Congratulation/Congratulation.java
@@ -0,0 +1,22 @@
+package Congratulation;
+
+import java.util.Scanner;
+
+public class Congratulation {
+    public static void main(String[] args){
+        System.out.println("Сегодня твой праздник?");
+        String str = new Scanner(System.in).nextLine();
+        if (str == "Да") {
+            System.out.println("С праздником!");
+        }else if (str == "Да")
+                    System.out.println("В другой раз");
+            }
+            }
+
+
+
+
+
+
+
+
